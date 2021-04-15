9c5a2ff861101abb0ce823506bc7b8ddbb549d9e
Bas <bas1328@mail.ru>
2021-04-15 09:21:19 +0300

    [;p

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 "use strict";
+
+const heading = document.querySelector(".heading-primary-main");
+const headingSub = document.querySelector(".heading-primary-sub");
+const button = document.getElementById("btn");
+let firstWord;
+let rest;
+
+const adviceFn = async function () {
+  const resp = await fetch(" 	https://api.adviceslip.com/advice");
+  const data = await resp.json();
+  //   console.log(data);
+  const adviceStr = data.slip.advice;
+  console.log(` INITIAL STRING IS: ${adviceStr}`);
+  firstWord = adviceStr.split(" ")[0];
+  //   console.log(firstWord);
+  rest = adviceStr.slice(firstWord.length, adviceStr.length);
+  console.log(rest);
+};
+
+adviceFn();
+
+button.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log(firstWord);
+  heading.innerHTML = firstWord;
+  headingSub.innerHTML = rest;
+});
